042f28da5b125e24b5234f7c20e2c7d3377bbd46
TheodoraDimitrova <teddybear901@abv.bg>
2019-07-12 00:16:22 +0300

    connect mongoDB with the app

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+//connect Database
+connectDB();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the employee records API...' })
@@ -9,8 +14,6 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/employees', require('./routes/employees'));
 app.use('/api/profile', require('./routes/profile'));
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
